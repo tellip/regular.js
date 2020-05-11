1b0fb52c07480b2e058ad5fce8dc6a30bbd860c7
Terrance Phillip <luojunyi.0@gmail.com>
2020-05-11 17:17:46 +0800

    asdf

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
-let check = require('./check');
+let type = require('./type');
 
-let f = check.overload(
-    [['number'], (a, b) => a + b],
+let f = type.overload(
+    [{0: Number, 1: 'number'}, (a, b) => a + b],
     [['string'], s => 'hello ' + s]
 );
 console.log(f(1, 2), f('asdf'));
--- src/type.js
+++ src/type.js
@@ -1,14 +1,14 @@
 module.exports = do {
     let check = (pattern, data) => pattern === null ? data === null : ({
         string: () => typeof data === pattern,
-        function: () => data instanceof pattern,
+        function: () => Object(data) instanceof pattern,
         object: () => ({
             Array: () => data.every(sub_data => pattern.some(sub_pattern => check(sub_pattern, sub_data))),
             Object: () => Reflect.ownKeys(pattern).every(key => check(pattern[key], data[key]))
         })[pattern.constructor.name]()
     })[typeof pattern]();
 
-    check.switch = (value, ...branches) => do {
+    let switch_ = (value, ...branches) => do {
         let rtn;
         branches.some(([pattern, callback]) => check(pattern, value) && do {
             rtn = callback();
@@ -17,9 +17,9 @@ module.exports = do {
         rtn;
     };
 
-    check.overload = (...polys) => (...args) => check.switch(args, ...polys.map(
-        ([[...pattern], callback]) => [pattern, () => callback(...args)]
+    let overload = (...polys) => (...args) => switch_(args, ...polys.map(
+        ([pattern, callback]) => [pattern, () => callback(...args)]
     ));
 
-    check;
+    ({check, switch: switch_, overload});
 };
